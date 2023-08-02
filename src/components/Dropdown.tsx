330735748cbdcf7b395bebc01b1202e22565af67
RohanAli12 <alirohan500@gmail.com>
2023-08-02 20:36:14 +0500

    commit

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -19,17 +19,17 @@ const Dropdown = () => {
       {dropdownOpen && (
         <ul className="absolute right-0 mt-2 py-2 w-48 bg-white rounded shadow-lg">
           <li>
-            <Link className="block text-xl px-4 py-2 text-gray-800 hover:bg-gray-200" href="/Men">
+            <Link className="block text-xl px-4 py-2 text-gray-800 hover:bg-gray-200" href="/Products/Men">
               Men
             </Link>
           </li>
           <li>
-            <Link className="block px-4 text-xl py-2 text-gray-800 hover:bg-gray-200" href="/Women">
+            <Link className="block px-4 text-xl py-2 text-gray-800 hover:bg-gray-200" href="/Products/Women">
               Women
             </Link>
           </li>
           <li>
-            <Link className="block px-4 py-2 text-xl text-gray-800 hover:bg-gray-200" href="/AllProducts">
+            <Link className="block px-4 py-2 text-xl text-gray-800 hover:bg-gray-200" href="/Products/AllProducts">
               All Products
             </Link>
           </li>
