a300ff88c9d3d7882742c3253485aa8f9109f1ba
RohanAli12 <alirohan500@gmail.com>
2023-08-02 06:49:39 +0500

    updated

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react'
 
 const Button:FC<{text:string}> = ({text}) => {
   return (
-    <Link href="/AllProducts">
+    <Link href="/Products/AllProducts">
         <button  className="bg-gradient-to-r from-blue-500 to-purple-700 h-10 md:h-14 w-44 md:w-60 hover:shadow-lg duration-300 hover:scale-105 text-sm  text-white font-semibold  md:text-lg  sm:px-4 sm:py-3  rounded-full px-3 py-2  hover:bg-[#808080]" >{text}</button>
     </Link>
   )
