42d742c7128becbc4f575fe44d7e87de360a1efe
RohanAli12 <alirohan500@gmail.com>
2023-08-02 07:05:24 +0500

    comit

--- src/app/Products/[category]/page.tsx
+++ src/app/Products/[category]/page.tsx
@@ -14,32 +14,28 @@ const page = async ({ params }: { params: { category: string } }) => {
             <Wrapper>
                 <div className=" grid  md:grid-cols-[repeat(3,auto)] grid-rows-[repeat(3,auto)]  justify-center  gap-10">
                     {params.category !== "AllProducts" &&
-                        filterData.map((items: ProductInterface) => (
-                            <>
-                            <Link href={`/AllProductsInfo/${items._id}`}>
+                        filterData.map((items: ProductInterface) => (         
+                            <Link key={items._id} href={`/AllProductsInfo/${items._id}`}>
                                 <div className="max-w-xl rounded overflow-hidden bg-[#E5E4E2] flex-col  flex shadow-lg  hover:scale-105  hover:duration-300 hover:ease-linear ">
                                     <Image src={urlForImage(items.image).url()} className='max-h-[280px] object-cover object-center ' width={400} height={400} alt="Upcomming Events" />
                                     <strong className="text-slate-700 font-extrabold text-2xl">{items.title}</strong>
                                     <div className="font-semibold text-xl mb-2">${items.price}</div>
                                 </div>
                             </Link>
-                            </>
+                            
                         ))
                     }
                     {params.category === "AllProducts" &&
                         data.map((items: ProductInterface) => (
-                            <>
-                            <Link href={`/AllProductsInfo/${items._id}`}>
+                            <Link key={items._id} href={`/AllProductsInfo/${items._id}`}>
                                 <div className="max-w-xl rounded overflow-hidden bg-[#E5E4E2] flex-col  flex shadow-lg  hover:scale-105  hover:duration-300 hover:ease-linear ">
                                     <Image src={urlForImage(items.image).url()} className='max-h-[280px] object-cover object-center ' width={400} height={400} alt="Upcomming Events" />
                                     <strong className="text-slate-700 font-extrabold text-2xl">{items.title}</strong>
                                     <div className="font-semibold text-xl mb-2">${items.price}</div>
                                 </div>
                                 </Link>
-                            </>
                         ))
                     }
-
                 </div>
             </Wrapper>
         </section>
